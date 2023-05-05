f05cebcbc74db2d27bb7ff04cbfe4dffe24c7b05
Vishalb073 <117665872+Vishalb073@users.noreply.github.com>
2023-05-05 12:00:10 +0530

    Assignment Done

--- src/main/java/com/driver/model/User.java
+++ src/main/java/com/driver/model/User.java
@@ -13,26 +13,32 @@ public class User {
     }
 
     public int getaadharCardNo() {
+
         return aadharCardNo;
     }
 
     public void setaadharCardNo(int aadharCardNo) {
+
         this.aadharCardNo = aadharCardNo;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public int getAge() {
+
         return age;
     }
 
     public void setAge(int age) {
+
         this.age = age;
     }
 }
